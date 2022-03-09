9a6d58d8e98d4bd9a3ef4a721e3d2f4cbc9160d8
jason <jduong89@gmail.com>
2022-03-08 19:36:39 -0800

    adds about method

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -2,4 +2,8 @@ class WelcomesController < ApplicationController
   def hello_method
     render json: { message: "Hello from welcomes controller!" }
   end
+
+  def about_method
+    render json: { message: "Learn all about me!" }
+  end
 end
